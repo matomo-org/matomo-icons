2ad58208ead0c08777e8a7d90ecbc1144f6031c5
Lukas Winkler <l.winkler23@mailbox.org>
2017-01-03 10:21:55 +0100

    increase image size to 48x48
    except for smaller images

--- convert.sh
+++ convert.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 shopt -s globstar
-size=16
+size=48
 
 for i in src/**/*.{png,gif,jpg,ico}; do
-    echo "$i"
     absDirname=$(dirname "$i")
     origFilename=$(basename "$i")
     code=${origFilename%.*}
     dirname="dist/${absDirname#src/}"
     distFile="${dirname}/${code}.png"
-    echo "$distFile"
     if [ ! -d "$dirname" ]
     then
         mkdir -p "$dirname"
@@ -33,27 +31,43 @@ for i in src/**/*.{png,gif,jpg,ico}; do
     # if file (or symlink) -> didn't get deleted
     if [ -e "$i" ]
     then
-        convert \
-            "$i" \
-            -strip \
-            -transparent white \
-            -background none \
-            -trim \
-            -thumbnail ${size}x${size} \
-            -unsharp 0x1 \
-            -gravity center \
-            -extent ${size}x${size} \
-            "$distFile"
-            # input file
-            # strip metadata
-            # make background transparent
-            # keep transparency
-            # cut border
-            # get only one image from .ico
-            # resize while keeping the aspect ratio
-            # sharpen the image
-            # center image
-            # fit to 16x16
+        width=$(identify -ping -format "%w" "$i")
+        height=$(identify -ping -format "%h" "$i")
+        echo "(${width}x${height})"
+        if [[ $height -gt $size ]] && [[ $width -gt $size ]]
+        then
+            convert \
+                "$i" \
+                -strip \
+                -transparent white \
+                -background none \
+                -trim \
+                -thumbnail ${size}x${size}\> \
+                -unsharp 0x1 \
+                -gravity center \
+                -extent ${size}x${size} \
+                "$distFile"
+                # input file
+                # strip metadata
+                # make background transparent
+                # keep transparency
+                # cut border
+                # get only one image from .ico
+                # resize while keeping the aspect ratio
+                # sharpen the image
+                # center image
+                # fit to 16x16
+        else
+            convert \
+                "$i" \
+                -strip \
+                -transparent white \
+                -background none \
+                "$distFile"
+            echo -e "\033[31mWarning: This image is smaller than the default size (${width}x${height})"
+            echo -e "$i"
+            echo  -e "\033[0m"
+        fi
         # optimize png:
         pngquant -f --ext .png -s 1 --skip-if-larger --quality 70-95 "$distFile"
     fi
