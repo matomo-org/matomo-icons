ced86fb482671bbaa95305f0b009951c195c127b
Findus23 <l.winkler23@me.com>
2017-09-29 20:27:36 +0200

    better image difftool

--- tools/git-imgdiff.sh
+++ tools/git-imgdiff.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-# by http://varya.me/en/posts/image-diffs-with-git/
-compare $2 $1 png:- | montage -geometry +4+4 $2 - $1 png:- | gwenview /dev/stdin 
+# improved version of http://varya.me/en/posts/image-diffs-with-git/
+# enable with `git config diff.image.command 'tools/git-imgdiff.sh'`
+echo $1
+compare $2 $1 tmp/diff.png
+compare $2 $1 -fuzz 5% tmp/fuzzy.png
+compare $2 $1 -fuzz 10% tmp/fuzzier.png
 
+composite $2 $1 -compose difference tmp/comp.png
+convert tmp/comp.png -auto-level tmp/comp-auto.png
+
+montage -geometry +4+4 -tile 7x1 $1 tmp/diff.png tmp/fuzzy.png tmp/fuzzier.png tmp/comp.png tmp/comp-auto.png $2 tmp/final.png
+mogrify -magnify tmp/final.png
+display tmp/final.png
