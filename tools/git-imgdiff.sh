718ef699c6a834a1e175179a5e7d4c349ddbcc87
Lukas Winkler <l.winkler23@mailbox.org>
2018-03-01 19:15:01 +0100

    update flags

--- tools/git-imgdiff.sh
+++ tools/git-imgdiff.sh
@@ -11,4 +11,5 @@ convert tmp/comp.png -auto-level tmp/comp-auto.png
 
 montage -geometry +4+4 -tile 7x1 $1 tmp/diff.png tmp/fuzzy.png tmp/fuzzier.png tmp/comp.png tmp/comp-auto.png $2 tmp/final.png
 mogrify -magnify tmp/final.png
+cp tmp/final.png tmp/diffs/$(basename $1)
 display tmp/final.png
