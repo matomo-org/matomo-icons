2892920495ee118523b4b28b6ed7c1c65b600796
Lukas Winkler <git@lw1.at>
2024-07-21 00:50:40 +0200

    update conversion of all icons
    Unfortunately the newer versions of pngquant doesn't seem to result in
    binary-identical images as before anymore.
    So I updated directly to 3.x and updated all images at once.
    I quickly scrolled through the list of visual diffs and none of the
    icons changed visually

--- tools/git-imgdiff.sh
+++ tools/git-imgdiff.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # improved version of http://varya.me/en/posts/image-diffs-with-git/
 # enable with `git config diff.image.command 'tools/git-imgdiff.sh'`
-echo $1
-compare $2 $1 tmp/diff.png
-compare $2 $1 -fuzz 5% tmp/fuzzy.png
-compare $2 $1 -fuzz 10% tmp/fuzzier.png
+echo "$1"
+compare "$2" "$1" tmp/diff.png
+compare "$2" "$1" -fuzz 5% tmp/fuzzy.png
+compare "$2" "$1" -fuzz 10% tmp/fuzzier.png
 
-composite $2 $1 -compose difference tmp/comp.png
+composite "$2" "$1" -compose difference tmp/comp.png
 convert tmp/comp.png -auto-level tmp/comp-auto.png
 
-montage -geometry +4+4 -tile 7x1 $1 tmp/diff.png tmp/fuzzy.png tmp/fuzzier.png tmp/comp.png tmp/comp-auto.png $2 tmp/final.png
+montage -geometry +4+4 -tile 7x1 "$1" tmp/diff.png tmp/fuzzy.png tmp/fuzzier.png tmp/comp.png tmp/comp-auto.png "$2" tmp/final.png
 mogrify -magnify tmp/final.png
-cp tmp/final.png tmp/diffs/$(basename $1)
+cp tmp/final.png "tmp/diffs/$(basename "$1")"
 # display tmp/final.png
